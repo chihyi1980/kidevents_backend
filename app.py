07b127a05c2d6343be903cd7aec514e15d1c206b
James Wang <chihyi1980@gmail.com>
2024-08-12 01:53:13 +0800

    update

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from routes.user_routes import user_bp
 from routes.events_routes import events_bp
 from config import Config
 
 app = Flask(__name__)
+CORS(app)  # 允许所有域访问所有路由
 app.config.from_object(Config)  # 加载配置
 jwt = JWTManager(app)
 
