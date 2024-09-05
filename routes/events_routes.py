de80638effade935401fead8b2a5b0640ec3b396
James Wang <chihyi1980@gmail.com>
2024-09-05 22:14:19 +0800

    update

--- routes/events_routes.py
+++ routes/events_routes.py
@@ -16,12 +16,25 @@ def add_event():
     local_time = utc_now.astimezone(pytz.timezone('Asia/Taipei'))
     data['created_at'] = local_time.isoformat()
     data['updated_at'] = local_time.isoformat()
+    data['is_online'] = 0
     insert_event(data)
     return jsonify({"msg": "Event added successfully"}), 201
 
 @events_bp.route('/events/list', methods=['GET'])
 def get_events():
     events = find_all_events()
-    sorted_events = sorted(events, key=lambda x: x.get('created_at', 0))
-    return jsonify([{**loc, "_id": str(loc["_id"])} for loc in sorted_events])        
+
+    # 遍歷每個事件，提取 events_tag 中的 _id 並存入 events_tag_ids
+    for event in events:
+        # 假設 events_tag 是一個列表，提取其中的每個 _id
+        if 'event_tag' in event:
+            event_tag_ids = [tag.get('_id') for tag in event['event_tag'] if '_id' in tag]
+            event['event_tag_ids'] = event_tag_ids
+
+    # 將事件按 created_at 日期從新到舊排序
+    sorted_events = sorted(events, key=lambda x: x.get('created_at', 0), reverse=True)
+
+    # 返回結果，並將 _id 轉換為字符串
+    return jsonify([{**loc, "_id": str(loc["_id"])} for loc in sorted_events])
+
 
