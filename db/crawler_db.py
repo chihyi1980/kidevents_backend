a6a390052ecf046c98880b169f035b3c9b8ec1c5
James Wang <chihyi1980@gmail.com>
2024-09-29 16:19:11 +0800

    add crawler config routes & db

--- db/crawler_db.py
+++ db/crawler_db.py
@@ -4,7 +4,25 @@ from config import Config
 
 client = MongoClient(Config.MONGO_URI)
 crawler_db = client['crawler_db']
+
+#紀錄已經爬娶過的url
 used_url_collection = crawler_db['used_url']
+
+#爬蟲設置JSON
+#範例
+"""
+{
+    'host' : 'https://tam.gov.taipei/',  //爬取主網址
+    'list_url': 'News.aspx?n=49854D6439EC40C0&sms=72544237BBE4C5F6',  //列表頁
+    'event_url_p': 'News_Content.aspx',  //每個 event page的抓取特徵值
+    'encoding' : 'utf-8',  //網頁編碼
+    'target_id' : 'CCMS_Content',   //活動內容div id
+    'event_loc' : '臺北市',   //地區
+    'event_org' : '台北市立天文科學教育館'   //活動機構名稱
+}
+"""
+crawler_confing_collection = crawler_db['crawler_confing']
+
 
 def insert_used_url(url):
     return used_url_collection.insert_one(url)
@@ -13,3 +31,9 @@ def is_url_exist(url):
     query = {"value": url}
     result = used_url_collection.find_one(query)
     return result is not None
+
+def insert_crawler_config(config):
+    return crawler_confing_collection.insert_one(config)
+
+def find_all_crawler_config():
+    return list(crawler_confing_collection.find())
--- routes/crawler_routes.py
+++ routes/crawler_routes.py
@@ -1,6 +1,6 @@
 from datetime import datetime 
 from flask import Blueprint, request, jsonify
-from db.crawler_db import insert_used_url, is_url_exist
+from db.crawler_db import insert_used_url, is_url_exist, insert_crawler_config, find_all_crawler_config
 
 crawler_bp = Blueprint('crawler_bp', __name__)
 
@@ -22,3 +22,17 @@ def check_used_url():
     else:
         return jsonify({'value': False}), 201
     
+#新增 crawler config
+@crawler_bp.route('/crawler/config/create', methods=['POST'])
+def add_config():
+    data = request.get_json()
+    insert_crawler_config(data)
+    return jsonify({"msg": "crawler config added successfully"}), 201
+
+#取得 crawler config 列表
+@crawler_bp.route('/crawler/config/list', methods=['GET'])
+def get_config():
+    configs = find_all_crawler_config()
+    
+    # 返回結果，並將 _id 轉換為字符串
+    return jsonify([{**con, "_id": str(con["_id"])} for con in configs])
